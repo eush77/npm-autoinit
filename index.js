7c434753004bed2666098d7dcc7be60834e079ef
Eugene Sharygin <eush77@gmail.com>
2015-08-11 02:37:55 +0300

    Fix package.json warnings

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 var init = require('init-package-json'),
     findRoot = require('find-root');
 
+var fs = require('fs'),
+    path = require('path');
+
 
 module.exports = function (dir, cb) {
   // Make sure no package.json is there yet.
@@ -17,6 +20,17 @@ module.exports = function (dir, cb) {
   console.log = Function.prototype;
   init(dir, String(), { yes: true }, function (err) {
     console.log = consoleLog;
-    cb(err);
+    if (err) return cb(err);
+    var pkgFile = path.join(dir, 'package.json');
+    var pkg = fixup(require(pkgFile));
+    fs.writeFile(pkgFile, JSON.stringify(pkg, null, 2), cb);
   });
 };
+
+
+// Fix package.json to not produce warnings.
+function fixup (pkg) {
+  pkg.description = ' ';
+  pkg.repository = {};
+  return pkg;
+}
